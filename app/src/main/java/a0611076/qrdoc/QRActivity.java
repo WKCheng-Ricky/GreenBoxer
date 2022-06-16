2b8152611dcb37efdda93fcc77fed61b14491498
Wai Kiu Cheng <rickychengkiu@gmail.com>
2022-06-17 05:45:27 +0800

    Added button for QR scanning

--- app/src/main/java/a0611076/qrdoc/QRActivity.java
+++ app/src/main/java/a0611076/qrdoc/QRActivity.java
@@ -9,6 +9,6 @@ public class QRActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr);
+        setContentView(R.layout.activity_qractivity);
     }
 }
